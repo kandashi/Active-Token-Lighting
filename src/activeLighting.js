de2c2708f4faefb6606bbfea0751ce978068edb3
kandashi <jacobmpalmer@live.co.uk>
2021-01-22 13:45:55 +0000

    undefined fix

--- src/activeLighting.js
+++ src/activeLighting.js
@@ -37,7 +37,7 @@ function ReadUpdate(actorId, effect) {
         }
         if (ATLUpdate === false) return;
         let token = actor.getActiveTokens()[0];
-            performUpdate(token, LightFlag, SizeFlag, tokenData)
+        performUpdate(token, LightFlag, SizeFlag, tokenData)
     }, 20)
 }
 
@@ -68,67 +68,67 @@ function performUpdate(token, LightFlag, SizeFlag, tokenData) {
 
     let { dimLight, brightLight, dimSight, brightSight, sightAngle, lightColor, lightEffect, colorIntensity, lightAngle } = LightFlag !== undefined ? LightFlag : 0;
 
-        if (LightFlag?.preset === 'torch') {
-            dimLight = "40";
-            brightLight = "20";
-            lightColor = "#a2642a";
-            lightEffect = {
-                'type': 'torch',
-                'speed': 1,
-                'intensity': 1
-            };
-            colorIntensity = "0.4"
-        }
-        if (LightFlag?.preset === "lantern") {
-            dimLight = "60";
-            brightLight = "30";
-            lightColor = "#a2642a";
-            lightEffect = {
-                'type': 'torch',
-                'speed': 1,
-                'intensity': 1
-            };
-            colorIntensity = "0.4"
-        }
-        if (LightFlag?.preset === "candle") {
-            dimLight = "10";
-            brightLight = "2";
-            lightColor = "#a2642a";
-            lightEffect = {
-                'type': 'torch',
-                'speed': 1,
-                'intensity': 1
-            };
-            colorIntensity = "0.2"
-        }
+    if (LightFlag?.preset === 'torch') {
+        dimLight = "40";
+        brightLight = "20";
+        lightColor = "#a2642a";
+        lightEffect = {
+            'type': 'torch',
+            'speed': 1,
+            'intensity': 1
+        };
+        colorIntensity = "0.4"
+    }
+    if (LightFlag?.preset === "lantern") {
+        dimLight = "60";
+        brightLight = "30";
+        lightColor = "#a2642a";
+        lightEffect = {
+            'type': 'torch',
+            'speed': 1,
+            'intensity': 1
+        };
+        colorIntensity = "0.4"
+    }
+    if (LightFlag?.preset === "candle") {
+        dimLight = "10";
+        brightLight = "2";
+        lightColor = "#a2642a";
+        lightEffect = {
+            'type': 'torch',
+            'speed': 1,
+            'intensity': 1
+        };
+        colorIntensity = "0.2"
+    }
 
-        if (dimLight === undefined) dimLight = 0;
-        if (brightLight === undefined) brightLight = 0
-        if (dimSight === undefined) dimSight = 0
-        if (brightSight === undefined) brightSight = 0
-        if (sightAngle === undefined) sightAngle = 360
-        if (lightColor === undefined ) lightColor = ""
-        if (lightAngle === undefined ) lightAngle = 360
-        if (lightEffect === undefined && !LightFlag?.preset) lightEffect = tokenData.lightAnimation.type
-        if (colorIntensity === undefined) colorIntensity = tokenData.lightAlpha
+    if (dimLight === undefined) dimLight = 0;
+    if (brightLight === undefined) brightLight = 0
+    if (dimSight === undefined) dimSight = 0
+    if (brightSight === undefined) brightSight = 0
+    if (sightAngle === undefined) sightAngle = 360
+    if (lightColor === undefined) lightColor = ""
+    if (lightAngle === undefined) lightAngle = 360
+    if (lightEffect === undefined && !LightFlag?.preset) lightEffect = tokenData.lightAnimation.type
+    if (colorIntensity === undefined) colorIntensity = tokenData.lightAlpha
 
-        let { size, scale } = SizeFlag !== undefined ? SizeFlag : 0;
-        if (size === undefined) size = tokenData.height
-        if (scale === undefined) scale = tokenData.scale
+    let { size, scale } = SizeFlag !== undefined ? SizeFlag : 0;
+    if (size === undefined) size = tokenData.height
+    if (scale === undefined) scale = tokenData.scale
 
 
-        let newDimLight = tokenData.dimLight > dimLight ? tokenData.dimLight : dimLight;
-        let newBrightLight = tokenData.brightLight > brightLight ? tokenData.brightLight : brightLight;
-        let newDimSight = tokenData.dimSight > dimSight ? tokenData.dimSight : dimSight;
-        let newBrightSight = tokenData.brightSight > brightSight ? tokenData.brightSight : brightSight;
-        let newSightAngle = tokenData.sightAngle > sightAngle ? sightAngle : tokenData.sightAngle;
-        let lightAnimation;
+    let newDimLight = tokenData.dimLight > dimLight ? tokenData.dimLight : dimLight;
+    let newBrightLight = tokenData.brightLight > brightLight ? tokenData.brightLight : brightLight;
+    let newDimSight = tokenData.dimSight > dimSight ? tokenData.dimSight : dimSight;
+    let newBrightSight = tokenData.brightSight > brightSight ? tokenData.brightSight : brightSight;
+    let newSightAngle = tokenData.sightAngle > sightAngle ? sightAngle : tokenData.sightAngle;
+    let lightAnimation;
 
-        if (lightEffect === "") lightAnimation = tokenData.lightAnimation
-        else if (LightFlag?.preset) lightAnimation = lightEffect;
-        else lightAnimation = JSON.parse(lightEffect)
+    if (lightEffect === undefined || lightEffect === "") lightAnimation = tokenData.lightAnimation
+    else if (LightFlag?.preset) lightAnimation = lightEffect;
+    else lightAnimation = JSON.parse(lightEffect)
 
-        token.update({ "lightAnimation": lightAnimation, dimLight: newDimLight, brightLight: newBrightLight, dimSight: newDimSight, brightSight: newBrightSight, lightColor: lightColor, sightAngle: newSightAngle, lightAlpha: (colorIntensity * colorIntensity), height: size, width: size, scale: scale, lightAngle: lightAngle })
+    token.update({ "lightAnimation": lightAnimation, dimLight: newDimLight, brightLight: newBrightLight, dimSight: newDimSight, brightSight: newBrightSight, lightColor: lightColor, sightAngle: newSightAngle, lightAlpha: (colorIntensity * colorIntensity), height: size, width: size, scale: scale, lightAngle: lightAngle })
 
 }
 
